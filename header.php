3bd2ddb2973124e932fd6e34a122c5312e8b5c9a
mAAdhaTTah <jamesorodig@gmail.com>
2014-04-21 21:11:51 -0400

    Style navbar

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
-		<nav id="site-navigation" class="main-navigation" role="navigation">
+		<nav id="site-navigation" role="navigation">
 			<h1 class="menu-toggle"><?php _e( 'Menu', 'lcarzs' ); ?></h1>
 			<div class="skip-link"><a class="screen-reader-text" href="#content"><?php _e( 'Skip to content', 'lcarzs' ); ?></a></div>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary',
